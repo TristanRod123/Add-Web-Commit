c3f426098a9d431bd6817f08130700113d617fbe
TristanRod123 <100972653+TristanRod123@users.noreply.github.com>
2022-04-21 15:01:55 -0500

    Add databases project

---
+++ Games-database/Sandbox-create-update-delete-queries.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    sandbox(int16_value,double_value,datetime_value)
+VALUES
+    (-235, 1557.2467, '2030-02-15 13:18:56')
+
+UPDATE
+    sandbox
+SET
+    int32_value = 78498429,
+    bool_value = 0
+WHERE
+    id = 28
+
+DELETE FROM sandbox WHERE id = 50
